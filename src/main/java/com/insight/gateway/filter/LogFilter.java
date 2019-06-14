f4e530a3761a2b93c90c131df4492ddf86f7bc78
Brian <xan.brian@gmail.com>
2019-06-14 13:29:40 +0800

    完成请求审计过滤器

--- src/main/java/com/insight/gateway/filter/LogFilter.java
+++ src/main/java/com/insight/gateway/filter/LogFilter.java
@@ -1,12 +1,11 @@
 package com.insight.gateway.filter;
 
-
 import com.insight.util.Generator;
 import com.insight.util.Json;
+import com.insight.util.Util;
 import com.insight.util.pojo.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -22,9 +21,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
 /**
@@ -45,31 +43,31 @@ public class LogFilter implements GlobalFilter, Ordered {
      */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-
-
-        MDC.put("requestId", Generator.uuid());
-        ServerHttpRequest request = exchange.getRequest();
         Log log = new Log();
         log.setTime(new Date());
         log.setLevel("DEBUG");
 
         // 读取客户端IP地址、请求方法和调用的接口URL
-
+        ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders headers = request.getHeaders();
         HttpMethod method = request.getMethod();
         RequestPath path = request.getPath();
-        log.setSource("");
-        log.setMethod(Objects.requireNonNull(method).name());
+        String source = getIp(headers);
+        if (source == null || source.isEmpty()) {
+            source = request.getRemoteAddress().getAddress().getHostAddress();
+        }
+
+        String fingerprint = Util.md5(source + headers.getFirst("user-agent"));
+        request.mutate().header("fingerprint", fingerprint).build();
+        request.mutate().header("requestId", Generator.uuid()).build();
+
+        log.setSource(source);
+        log.setMethod(method.name());
         log.setUrl(path.value());
-
-        // 读取请求头
-        Map<String, String> headers = new HashMap<>(16);
-        HttpHeaders headerList = request.getHeaders();
-
-        log.setHeaders(headers);
+        log.setHeaders(headers.toSingleValueMap());
 
         // 读取请求参数
         MultiValueMap<String, String> params = request.getQueryParams();
-
         log.setParams(params.isEmpty() ? null : params.toSingleValueMap());
 
         // 如请求方法为GET,则打印日志后结束
@@ -100,5 +98,32 @@ public class LogFilter implements GlobalFilter, Ordered {
     @Override
     public int getOrder() {
         return 1;
+    }
+
+    /**
+     * 获取客户端IP
+     *
+     * @param headers 请求头
+     * @return 客户端IP
+     */
+    private String getIp(HttpHeaders headers) {
+        if (headers.isEmpty()) {
+            return null;
+        }
+
+        AtomicReference<String> ip = new AtomicReference<>(headers.getFirst("X-Real-IP"));
+        if (ip.get() == null || ip.get().isEmpty()) {
+            ip.set(headers.getFirst("X-Forwarded-For"));
+        }
+
+        if (ip.get() == null || ip.get().isEmpty()) {
+            ip.set(headers.getFirst("Proxy-Client-IP"));
+        }
+
+        if (ip.get() == null || ip.get().isEmpty()) {
+            ip.set(headers.getFirst("WL-Proxy-Client-IP"));
+        }
+
+        return ip.get();
     }
 }
