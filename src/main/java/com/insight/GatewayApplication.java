d2d36d0372913fdd1d50d8c5f98d79b6d460e378
Brian <xan.brian@gmail.com>
2021-03-02 08:45:05 +0800

    eureka to consul

--- src/main/java/com/insight/GatewayApplication.java
+++ src/main/java/com/insight/GatewayApplication.java
@@ -2,7 +2,7 @@ package com.insight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -11,7 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @remark 应用入口程序
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 public class GatewayApplication {
 
