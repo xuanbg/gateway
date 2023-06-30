dcdba66486e5bd1e0371c76016e067f9bda67752
Brian Xan <xuan_bingan@hotmail.com>
2023-06-30 12:14:34 +0800

    fix bug

---
+++ src/main/java/com/insight/gateway/common/WebfluxConfig.java
@@ -0,0 +1,19 @@
+package com.insight.gateway.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * @author 宣炳刚
+ * @date 2023/6/30
+ * @remark
+ */
+@Configuration
+public class WebfluxConfig implements WebFluxConfigurer {
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024);
+    }
+}
