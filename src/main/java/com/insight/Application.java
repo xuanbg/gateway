f95293d63993e794086370873bb666154047d726
Brian <xan.brian@gmail.com>
2024-05-11 20:42:26 +0800

    支持更新授权信息

--- src/main/java/com/insight/Application.java
+++ src/main/java/com/insight/Application.java
@@ -3,6 +3,7 @@ package com.insight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author 宣炳刚
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class Application {
 
     /**
